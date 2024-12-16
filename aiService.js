46736e61e60a1fb34c9b0bf6ae1088e8c0fe11ce
ElectricSpectra <sohansoma2806@gmail.com>
2024-12-16 15:44:26 +0530

    Update aiService.js, new api key

--- aiService.js
+++ aiService.js
@@ -1,6 +1,6 @@
 export async function fetchGeminiAnswer(question) {
     console.log("fetchGeminiAnswer called with question:", question);
-    const apiKey = "AIzaSyBSHKbI4h16pNXvZD0EFr9t3Dqo-A5D-E4"; // Gemini API Key
+    const apiKey = "AIzaSyCfc4CJC0JhpsvnHI_fppINOS6hD7gsztk"; // Gemini API Key
     const apiUrl = "https://generativelanguage.googleapis.com/v1/models/gemini-1.0-pro:generateContent";
 
     try {
